acc44e73cc62645fb5da90ffdb1a9e60648e5baa
Nils Norman Haukås <nils@thunki.com>
2018-05-02 13:24:29 +0300

    Initial Commit

---
+++ WhatIsTheDose3/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WhatIsTheDose3
+//
+//  Created by Nils Norman Haukås on 02/05/2018.
+//  Copyright © 2018 Nils Norman Haukås. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WhatIsTheDose3/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WhatIsTheDose3
+//
+//  Created by Nils Norman Haukås on 02/05/2018.
+//  Copyright © 2018 Nils Norman Haukås. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
